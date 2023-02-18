f9703e31e29ad9a713bca9e6ed5d3bd90a7839e1
power <power@powerdeMacBook-Pro.local>
2023-02-18 23:32:06 +0800

    增加logo

--- src/components/LogoCom/LogoCom.vue
+++ src/components/LogoCom/LogoCom.vue
@@ -1,11 +1,41 @@
 <template>
-	<div>
-		logocom
-	</div>
+	<div class="logo" @click="toHome">
+	    <img src="@/assets/images/91huajian-caise.svg" />
+	  </div>
 </template>
 
-<script>
+<script setup lang="ts">
+	interface IBgcColor {
+		fontColor? : string,
+		bgColor? : string
+	}
+	withDefaults(defineProps<IBgcColor>(), {
+		fontColor: '#74a274',
+		bgColor: ''
+	})
+	const router = useRouter();
+	const toHome = () => {
+		router.push('/')
+	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .logo {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    user-select: none;
+    img {
+      width: 120px;
+    }
+    span {
+      letter-spacing: 2px;
+      font-size: 22px;
+      font-weight: 600;
+      font-family: monospace;
+      color: v-bind('fontColor');
+      margin-left: 10px;
+    }
+  }
 </style>
